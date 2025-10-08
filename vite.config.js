b547fda78a505126addc431241d96e2a9d65a7b0
karthik-excrin <karthik@excrin.com>
2025-10-08 18:11:24 +0530

    Fix Prisma client and ESBuild syntax errors

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  esbuild: {
+    target: 'esnext'
+  },
   plugins: [
     remix({
       ignoredRouteFiles: ["**/.*"],
+      serverDependenciesToBundle: ["@prisma/client"],
     }),
     tsconfigPaths(),
   ],
